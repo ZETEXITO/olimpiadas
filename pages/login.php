1be5ffcdd70017c0aa22ca42ef772092fbbfad66
Tomiroca <tomi.roca06@gmail.com>
2024-08-14 21:41:35 -0300

    carme login no terminado

---
+++ pages/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrito de compra - Ingreso</title>
+</head>
+<body class="bodylogin">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4"></div>
+
+                <div class="col-4 text-center pb-5">
+                <img class="w-25 h-auto" src="../assets/images/carrito.png" alt="hola">
+                </div>
+
+                <div class="col-4"></div>
+            </div>
+ 
+                    <H1 class="text-center pb-5">Iniciar sesión</H1>
+
+                    <div>
+                     <form class="text-center" action="">  
+
+
+
+
+
+                     
+                    </div>
+
+
+
+                       
+                        
+                </form>
+             </div>          
+            </div>
+
+        
+    </div>
+</body>
+</html>
