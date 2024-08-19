58fdd9be1fa14386a0ad7bf2d90a64be0abc021f
Tomiroca <tomi.roca06@gmail.com>
2024-08-19 16:08:23 -0300

    Ultima versión 19-8-24

--- base_de_datos/subir_usuario.php
+++ base_de_datos/subir_usuario.php
@@ -13,11 +13,14 @@ $Listo = $_POST["Listo"];
 $Nombre = $_POST["nombre"]; 
 $Email = $_POST["correo"]; 
 $Contra = $_POST["contraseña"];
-
+$_SESSION["Id_Usuario"] = "";
 if(isset($Listo)){
     $sql = "INSERT INTO usuarios ( Nombre, Email, Contraseña) VALUES ('$Nombre', '$Email', '$Contra')";
 }
-
+$query = mysqli($conn, "SELECT ID_Usuario FROM usuarios WHERE Nombre = $Nombre, Email = $Email, Contraseña = $Contra");
+while ($row = mysqli_fetch_array($query)){
+    $_SESSION["Id_Usuario"] = $row["ID_Usuario"];
+}
 if ($conn->query($sql) === TRUE){
     $_SESSION["registrado"]= 1 ;
     $_SESSION["Error"] = 0;
