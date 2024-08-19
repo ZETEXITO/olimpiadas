b110658983bead19e611bbefd0d3a389c4ca7f09
ZETEXITO <benites.ezequiel777@gmail.com>
2024-08-19 15:34:39 -0300

    responsive?

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,16 @@
 <link rel="stylesheet" href="../assets/css/style.css"> 
 <footer class="text-white py-4">
     <div class="card-footer">
-        <div class="container text-center ">
+        <div class="container text-center">
             <!-- Sección de enlaces -->
             <div class="row mb-3">
-                <div class="col-md-4">
+                <div class="col-lg-4">
                     <h5>Carrito de Compra</h5>
                 </div>
-                <div class="col-md-4">
+                <div class="col-lg-4">
                     <h5>Atención al Cliente</h5>
                 </div>
-                <div class="col-md-4">
+                <div class="col-lg-4">
                     <h5>Síguenos</h5>
                 </div>
             </div>
